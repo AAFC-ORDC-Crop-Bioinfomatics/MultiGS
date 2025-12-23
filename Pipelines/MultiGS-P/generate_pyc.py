9186b0bd46b311f9d057e84d206bfb0747d00b70
Chunfang Zheng <chunfang.zheng@canada.ca>
2025-12-22 21:08:51 -0500

    compiled script

--- Pipelines/MultiGS-P/generate_pyc.py
+++
@@ -1,2 +0,0 @@
-import py_compile
-py_compile.compile('/isilon/projects/J-002035_flaxgenomics/J-001386Frank_Lab/Flax_GS_project_J-003426/github/MultiGS-P/src/MultiGS-P_1.0.py', cfile='/isilon/projects/J-002035_flaxgenomics/J-001386Frank_Lab/Flax_GS_project_J-003426/github/MultiGS-P/pipeline/MultiGS-P_1.0.pyc')
